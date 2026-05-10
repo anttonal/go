5b0f734373e3b9a6f488ef1c96af931d5cd7a201
Antton Alivuotila <alivuotila.antton@gmail.com>
2026-05-10 15:33:20 +0300

    lesson2

--- ch7/l02/main.go
+++ ch7/l02/main.go
@@ -1,4 +1,9 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
+
+func getSMSErrorString(cost float64, recipient string) string {
+	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' cannot be sent", cost, recipient)
 }
