5c57afd4d4e749c97f57c494ba8bcbf546858fab
Antton Alivuotila <alivuotila.antton@gmail.com>
2026-05-10 16:16:41 +0300

    ch7 done

--- ch7/l09/main.go
+++ ch7/l09/main.go
@@ -1,4 +1,17 @@
 package main
 
-func main() {
+import (
+	"errors"
+)
+
+func validateStatus(status string) error {
+	if status == "" {
+		return errors.New("status cannot be empty")
+	}
+
+	if len(status) > 140 {
+		return errors.New("status exceeds 140 characters")
+	}
+
+	return nil
 }
