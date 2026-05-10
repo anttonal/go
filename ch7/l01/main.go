c223fee3e0c13d91f9220cc5575f21735cba583f
Antton Alivuotila <alivuotila.antton@gmail.com>
2026-05-10 15:24:18 +0300

    ch7 l01

--- ch7/l01/main.go
+++ ch7/l01/main.go
@@ -1,4 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
+
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
+	c, err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0, err
+	}
+
+	s, err := sendSMS(msgToSpouse)
+	if err != nil {
+		return 0, err
+	}
+
+	return c + s, nil
+
+}
+
+// don't edit below this line
+
+func sendSMS(message string) (int, error) {
+	const maxTextLen = 25
+	const costPerChar = 2
+	if len(message) > maxTextLen {
+		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+	}
+	return costPerChar * len(message), nil
 }
